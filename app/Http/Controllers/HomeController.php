d627eb44dc8df6f15cd2a74441a7467d0ca578df
Gurdeep Singh <gurdeepsingh2000329@gmail.com>
2021-09-12 15:41:14 +0200

    finita parte layout

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,6 +9,6 @@ class HomeController extends Controller
 {
     public function index(){
         $allTrips = Trip::all();
-        dump($allTrips);
+        return view('HomePage',compact('allTrips'));
     }
 }
---
+++ resources/views/templates/header.blade.php
@@ -0,0 +1,31 @@
+<div class="navbar">
+    <div class="logo-container">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Trip.com_logo.svg/1200px-Trip.com_logo.svg.png" alt="">
+    </div>
+    
+    <div class="menu-container">
+        <ul>
+            <li>Voli</li>
+            <li>Hotel</li>
+            <li>Treni</li>
+            <li>Business</li>
+            <li>Offerte</li>
+        </ul>
+    </div>
+
+    <div class='user-language'>
+        <span>Seleziona Lingua</span>
+        <select name="country-language" id="">
+            <option value="it">Italiano</option>
+            <option value="en">English</option>
+            <option value="es">Espanol</option>
+            <option value="de">Deutesche</option>
+        </select>
+    </div>
+
+    <div class="user-sign">
+        <button>Accedi al tuo account / Crea un Account</button>
+    </div>
+    
+
+</div>
